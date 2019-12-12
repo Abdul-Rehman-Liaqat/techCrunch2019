4fcb935fc2ead8bca35a4130ae00baa7e93d36df
Abdul Rehman Liaqat <arl@plista.com>
2019-12-12 18:38:06 +0100

    add code

--- src/app.py
+++ src/app.py
@@ -6,33 +6,20 @@ Created on Thu Dec 12 01:21:33 2019
 @author: abdulliaqat
 """
 
-
-from flask_restplus import Resource, Api
-from flask import Flask, jsonify
-import time
-
-# app = Flask(__name__)
-# api = Api(app)
-
-
-#
-# @api.route('/get_recommendations')
-# class Recommend(Resource):
-#     def get(self):
-#         recommendations = {
-#             "weather" : "The weather is humid and cold today. You will not see much sunlight",
-#             "clothes" : "Try wearing winter jacket with neck muffler",
-#             "Sleep"   : "You had 6 hours sleep which is less for you so try drinking tea too",
-#             "Pollution" : "AQI index is high today and exercising indoor is recommended",
-#             "Psychological" : "With the data for past few days, you seem a bit stressed. How about a round of Meditation"
-#                               "I can schedule one for you",
-#         }
-#         return jsonify(recommendations)
-#
-#
-# if __name__ == '__main__':
-#     app.run(host='127.0.0.1', port=5000, debug=True)
-# #    app.run(debug=True)
+import dash
+from dash.dependencies import Output, Input
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly
+import random
+import plotly.graph_objs as go
+from collections import deque
+from datetime import datetime
+now = datetime.now().strftime("%H:%M:%S")
+X = deque(maxlen=20)
+X.append(now)
+Y = deque(maxlen=20)
+Y.append(70)
 
 import dash
 import dash_core_components as dcc
@@ -42,6 +29,16 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+
+
+
+# api_connection
+from get_apis_data import get_weather_data
+latitude = 52.497139
+longitude = 13.453778
+
+hourly_temperature = get_weather_data(location={"lat":latitude,"long":longitude})
+
 colors = {
     'background': '#111111',
     'text': '#7FDBFF'
@@ -49,70 +46,61 @@ colors = {
 
 app.layout = html.Div(style={'backgroundColor': 'white', 'textAlign': 'center'}, children=[
     html.H1(children='HealthSherpa'),
-    html.Div(children='Your Daily Health Partner'),
-
-    html.Div(children='Recommendations', id='recommendations'),
-    html.Div(dcc.Input(id='input-box', type='text')),
-    html.Button('Submit', id='button'),
-    html.Div(id='output-container-button',children='Enter a value and press submit'),
-
-    dcc.Graph(
-        figure=dict(
-            data=[
-                dict(
-                    x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
-                       2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
-                    y=[16, 13, 10, 11, 28, 37, 43, 55, 56, 88, 105, 156, 270,
-                       299, 340, 403, 549, 499],
-                    name='HeartBeat',
-                    marker=dict(
-                        color='rgb(26, 118, 255)'
-                    )
-                )
-            ],
-            layout=dict(
-                title='Live HeartBeat',
-                showlegend=True,
-                legend=dict(
-                    x=0,
-                    y=1.0
-                ),
-                margin=dict(l=40, r=0, t=40, b=30)
-            )
-        ),
-        style={'height': 300},
-        id='my-graph'
+    html.H3(children='Your Daily Health Partner'),
+    html.Div(children='weather : The weather is humid and cold today. You will not see much sunlight'),
+    html.Div(children='clothes : Try wearing winter jacket with neck muffler'),
+    html.Div(children='Sleep : You had 6 hours sleep which is less for you so try drinking tea too'),
+    html.Div(children='Pollution : AQI index is high today and exercising indoor is recommended'),
+    html.Div(children='Psychological : With the data for past few days, you seem a bit stressed. How about a round of Meditation. I can schedule one for you'),
+    dcc.Graph(id='live-update-graph'),
+    dcc.Interval(
+        id='interval-component',
+        interval=2.5 * 1000,  # in milliseconds
+        n_intervals=0
     ),
+
 
     dcc.Graph(
         id='Weather Forecast',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': list(hourly_temperature.keys()), 'y': list(hourly_temperature.values()), 'type': 'bar', 'name': 'SF'},
             ],
             'layout': {
-                'title': 'Dash Data Visualization'
+                'title': 'Current Weather'
             }
         }
     )
 ])
 
-@app.callback(
-    dash.dependencies.Output('output-container-button', 'children'),
-    [dash.dependencies.Input('button', 'n_clicks')],
-    [dash.dependencies.State('input-box', 'value')])
-def update_output(n_clicks, value):
-    return get_recommendations()
+
+@app.callback(Output('live-update-graph', 'figure'),
+              [Input('interval-component', 'n_intervals')])
+def update_graph_scatter(n):
+    X.append(datetime.now().strftime("%H:%M:%S"))
+    Y.append(Y[-1]+Y[-1]*random.uniform(-0.01,0.01))
+
+    data = plotly.graph_objs.Scatter(
+            x=list(X),
+            y=list(Y),
+            name='Heart Beats-per-minute',
+            mode= 'lines+markers'
+            )
+
+    return {'data': [data],'layout' : go.Layout(xaxis=dict(range=[X[0],X[-1]]),
+                                                yaxis=dict(range=[min(Y),max(Y)]),
+                                                title='<b>Live HeartBeat</b>',
+                                                showlegend=True,
+                                                legend=dict(
+                                                    x=0,
+                                                    y=1.0
+                                                ),
+                                                margin=dict(l=40, r=0, t=40, b=30)
+                                                )}
 
 
-def get_recommendations():
-    recommendation = ("The weather is humid and cold today. You will not see much sunlight\n"+
-        "Try wearing winter jacket with neck muffler\n"+
-        "You had 6 hours sleep which is less for you so try drinking tea too\n"+
-        "AQI index is high today and exercising indoor is recommended\n"+
-        "With the data for past few days, you seem a bit stressed. How about a round of Meditation\n"+
-                          "I can schedule one for you")
-    return recommendation
+
+
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0',debug=True)
--- tests/test_get_apis_data.py
+++ tests/test_get_apis_data.py
@@ -1,7 +1,11 @@
 from get_apis_data import get_pollution_data, get_thryve_data, get_weather_data
 import os
 
-print(get_pollution_data(city="Berlin"))
-print(get_pollution_data(location={"lat":52.554192,"long":13.344960}))
-print(get_thryve_data(os.path.abspath(os.path.join(os.getcwd(),"data/activity_4325638680.csv"))).head())
-print(get_weather_data(location = {"lat":52.554192,"long":13.344960}))
+latitude = 52.497139
+longitude = 13.453778
+
+
+# print(get_pollution_data(city="Berlin"))
+# print(get_pollution_data(location={"lat":latitude,"long":longitude}))
+# print(get_thryve_data(os.path.abspath(os.path.join(os.getcwd(),"data/activity_4325638680.csv"))).head())
+print(get_weather_data(location = {"lat":latitude,"long":longitude}))
