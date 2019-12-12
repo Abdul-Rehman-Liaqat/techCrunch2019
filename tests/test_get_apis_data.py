c1bc17a2f7dfe97a6928129f06e25f7bd8de5e6d
Abdul Rehman Liaqat <arl@plista.com>
2019-12-12 01:00:01 +0100

    add get_thryve_data

--- tests/test_get_apis_data.py
+++ tests/test_get_apis_data.py
@@ -1,7 +1,6 @@
-from get_apis_data import get_pollution_data
+from get_apis_data import get_pollution_data, get_thryve_data
 import os
 
 print(get_pollution_data(city="Berlin"))
 print(get_pollution_data(location={"lat":52.554192,"long":13.344960}))
-
-
+print(get_thryve_data(os.path.abspath(os.path.join(os.getcwd(),"data/activity_4325638680.csv"))).head())
